f9360de1cdf5328c3bde9e0de63da9e6cbc016be
Bryan Cuevas <bryancuevas4@gmail.com>
2020-05-28 22:45:10 -0700

    A quick demo app using Twilio for the ELC High School Career Fair

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const express = require('express');
+const axios = require('axios');
+const { urlencoded } = require('body-parser');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const app = express();
+app.use(urlencoded({ extended: false}));
+
+const url = 'https://pokeapi.co/api/v2/pokemon';
+const pokemonCount = 151;
+
+app.post('/sms', async (req, res) => {
+    const twiml = new MessagingResponse();
+
+    console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
+
+    const pokemonData = await fetchPokemonData(generateRandomNumber());
+    const pokemonName = pokemonData.name;
+    twiml
+        .message(`You got: ${pokemonName.toUpperCase()}!`)
+        .media(`${pokemonData.front_default}`);
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
+
+http.createServer(app).listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * Math.floor(pokemonCount));
+}
+
+const fetchPokemonData = async (pokeNumber) => {
+    try {
+        const { data: {name, sprites: {front_default}}} = await axios.get(`${url}/${pokeNumber}`);
+        const pokemonData = { name, front_default };
+        return pokemonData;
+    } catch(error) {
+        console.log(error);
+    }
+}
